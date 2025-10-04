ccfeae9a850bc69b8e1a4a29d063dd190c8e48d7
Gustavo Lima01 <gustahlima@hotmail.com>
2025-10-04 18:05:36 -0300

    Adicionando stages ao tests

--- tests/login.test.js
+++ tests/login.test.js
@@ -3,8 +3,14 @@ import { sleep, check } from 'k6';
 
 export const options = {
   //iterations: 50, //quantidade de iterações que vai executar
-  vus:10, //quantidade de usuários virtuais que vão executar o teste, substitui o iterations acima
-  duration: '30s', //tempo que o teste vai durar (substitui o iterations acima)
+  // vus:10, //quantidade de usuários virtuais que vão executar o teste, substitui o iterations acima
+  // duration: '30s', //tempo que o teste vai durar (substitui o iterations acima)
+
+  stages: [ //simula o comportamento real dos usuários  
+    { duration: '5s', target: 10 }, //2 minutos para subir até 10 usuários
+    { duration: '20s', target: 10 }, //manter 10 usuários por 5 minutos
+    { duration: '5s', target: 0 } //2 minutos para descer até 0 usuários
+  ],
   thresholds: { //metas de performance alinhada com o time
     http_req_duration: ['p(90)<3000', 'max<5000'], //90% das requisições devem ser menores que 10ms e o tempo máximo de resposta deve ser menor que 1s
     http_req_failed: ['rate<0.01'], //taxa de erro, menor que 1%
