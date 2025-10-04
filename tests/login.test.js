5a380ea2cc82c6688d149a88647ddf1449b89378
Gustavo Lima01 <gustahlima@hotmail.com>
2025-10-04 17:27:52 -0300

    Adicionando arquivo inicial com teste de login

---
+++ tests/login.test.js
@@ -0,0 +1,39 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  iterations: 50, //quantidade de iterações que vai executar
+  thresholds: { //metas de performance alinhada com o time
+    http_req_duration: ['p(90)<10', 'max<1'], //90% das requisições devem ser menores que 10ms e o tempo máximo de resposta deve ser menor que 1s
+    http_req_failed: ['rate<0.01'], //taxa de erro, menor que 1%
+  }
+};
+
+export default function () {
+    //teste do login
+    const url = 'http://localhost:3000/login';
+
+    const payload = JSON.stringify({ //transforma o objeto em json
+        username: 'julio.lima',
+        senha: '123456',
+    });
+
+    const params = {
+        headers: {
+        'Content-Type': 'application/json',
+        },
+    };
+
+    const res = http.post(url, payload, params);
+    // console.log(resposta)
+
+    //check = no K6 servem só para informar se a API está ok
+    check(res, {
+      //'Validar que o Status é 200' = propriedade = é o nome do que quero testar
+      'Validar que o Status é 200': (r)  => r.status === 200,
+      'Validar que o token é string': (r) => typeof(r.json().token) === 'string',
+    })
+
+    sleep(1);      
+
+}
